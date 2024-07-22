92954c43dd752b759e60cd84701939d28f064d75
hudson-newey <grath@duck.com>
2024-07-22 20:54:00 +1000

    Add install sub-command

--- src/commands/customCommands.go
+++ src/commands/customCommands.go
@@ -8,7 +8,7 @@ import (
 
 func IsCustomCommand(command []string) bool {
 	switch command[1] {
-	case "explore":
+	case "explore", "install":
 		return true
 	}
 
@@ -20,7 +20,15 @@ func ExecuteCustomCommand(command string) {
 
 	switch splitCommand[0] {
 	case "explore":
-		ExploreRepo(splitCommand[1])
+		{
+			ExploreRepo(splitCommand[1])
+			break
+		}
+	case "install":
+		{
+			InstallRepo(splitCommand[1])
+			break
+		}
 	}
 }
 
@@ -35,4 +43,21 @@ func ExploreRepo(resourceUrl string) {
 	Execute("git clone " + resourceUrl + " " + tempDir)
 	Execute("yazi " + tempDir)
 	Execute("rm -rf " + tempDir)
+}
+
+func InstallRepo(resourceUrl string) {
+	repoName := strings.Split(resourceUrl, "/")
+	installLocation := "~/.local/bin/" + repoName[len(repoName)-1]
+
+	commandsToRun := []string{
+		"git clone " + resourceUrl + " " + installLocation,
+		"echo 'export PATH=$PATH:" + installLocation + "' >> ~/.bashrc",
+		"echo 'export PATH=$PATH:" + installLocation + "' >> ~/.zshrc",
+	}
+
+	for _, command := range commandsToRun {
+		Execute(command)
+	}
+
+	fmt.Println("Installed " + repoName[len(repoName)-1])
 }
