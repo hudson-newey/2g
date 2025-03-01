4173a2471e66f6b7c26a811c8b3b09e7653e9821
hudson-newey <grath@duck.com>
2025-03-01 22:37:17 +1000

    Make docs a build asset and add install task

---
+++ scripts/install_artifacts.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# this script will install 2g assets built on the local machine
+# you will have to build 2g before running this script
+#
+# you should be running this script through
+# $ task build && sudo task install
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Installer requires root privileges."
+    exit 1
+fi
+
+if [ $# -lt 1 ]; then
+    echo "Not enough arguments provided"
+    echo "Usage: scripts/install.sh <build_output>"
+    exit 2
+fi
+
+build_output=$1
+
+if [ ! -d $build_output ]; then
+    echo "could not find build output in $build_output"
+    echo "try building with the 'task build' command"
+    exit 3
+fi
+
+cp "./$build_output/2g" "/usr/local/bin/2g"
+cp "./$build_output/2g.1" "/usr/share/man/man1/2g.1"
